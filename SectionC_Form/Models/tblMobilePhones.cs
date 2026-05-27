30fd616effb8f45e59c5aa610a6bdd83e45eb3e7
Reefe Ivins <34356428@student.g.nwu.ac.za>
2026-05-27 10:55:57 +0200

    Added the base code to add, delete and find phones

---
+++ SectionC_Form/Models/tblMobilePhones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SectionC_Form.Models
+{
+    public class tblMobilePhones
+    {
+        string? Mobilecode { get; set; }
+
+        string? Make { get; set; }
+
+        int? Quantity { get; set; }
+    }
+}
